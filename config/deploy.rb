9987c793e779824f85c1e2d2a3a92a0ce9c58ff0
Michael Yockey <mike@heysparkbox.com>
2014-06-05 14:25:51 -0400

    Automatically cleaning up old releases

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,6 +35,7 @@ task :deploy => :environment do
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
+    invoke :'deploy:cleanup'
 
     to :launch do
       queue "touch #{deploy_to}/tmp/restart.txt"
